7abc551b3346d54282c07acf90aa7aaf6bef15e9
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-05 16:27:55 +0800

    organized views

--- guia_db/views/Museum.py
+++ guia_db/views/Museum.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Museum
-from .serializers import *
+from ..models import Museum
+from ..serializers import *
 
 # Create your views here.
 @api_view(['GET'])
