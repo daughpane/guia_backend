597e90f9f3cc8aea45b87ea21fd854af166d8f97
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-02-06 17:14:38 +0800

    Clean up code

--- guia_backend/settings.py
+++ guia_backend/settings.py
@@ -144,9 +144,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework_api_key.permissions.HasAPIKey",
         "rest_framework.permissions.IsAuthenticated"
-    ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    #   'rest_framework.authentication.BasicAuthentication',
-    #   'rest_framework.authentication.SessionAuthentication',
-    # )
+    ]
 }
