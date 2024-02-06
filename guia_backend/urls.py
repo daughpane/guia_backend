d8adc73bdb8fbaacc4077aad13d44adcda70a539
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-07 06:57:57 +0800

    Revert "vercel screen attempt 3"
    This reverts commit d1419a5300e1a3bddc80a8f27ac2bec190a35874.

--- guia_backend/urls.py
+++ guia_backend/urls.py
@@ -14,16 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 import guia_db.urls 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(guia_db.urls))
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
