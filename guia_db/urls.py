5e598336e27c3b16999dd13845474ab2434ed268
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-02-07 09:46:17 +0800

    remove simplejwt

--- guia_db/urls.py
+++ guia_db/urls.py
@@ -2,22 +2,13 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from rest_framework import routers
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+
 
 
 router = routers.DefaultRouter()
 
 urlpatterns = router.urls
 
-urlpatterns += [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
 
 urlpatterns += [
   path('museum/get/all', views.getAllMuseums),
