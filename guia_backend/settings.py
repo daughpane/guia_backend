77d0a8a2f450e3abf4fdd55cad759a04a4b1b01e
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-07 10:07:04 +0800

    Revert "fix vercel screen attempt 4"
    This reverts commit d57421c2e4db7926a035b25c4377b9b117f8d100.

--- guia_backend/settings.py
+++ guia_backend/settings.py
@@ -132,13 +132,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
-
-STATIC_URL = "static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
