2b87551a835b88c2b3b0f8f11059a56d2b2eb6e1
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-07 09:39:00 +0800

    fix vercel screen attempt 5

--- guia_backend/settings.py
+++ guia_backend/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
