540fe317a5e46d8fd4fdb5127127f20005d7bfc9
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-09 23:51:40 +0800

    url naming convention

--- guia_db/urls.py
+++ guia_db/urls.py
@@ -14,5 +14,5 @@ urlpatterns += [
   path('museum/get/all', views.getAllMuseums),
   path('admin/login', views.AdminLoginApiView.as_view()),
   path('admin/change-password', views.ChangePasswordApiView.as_view()),
-  path('create/artwork/', views.ArtworkCreateView.as_view()),
-  path('getAll/artworks/', views.ArtworkListView.as_view()),]
+  path('artwork/create', views.ArtworkCreateView.as_view()),
+  path('artwork/get/all', views.ArtworkListView.as_view()),]
