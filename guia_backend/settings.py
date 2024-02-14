76623f807d1ada9db9e7ad46624a3c0cd0db4519
Daughpane Reponte <daughpanereponte@gmail.com>
2024-02-14 11:53:48 +0800

    artwork image upload to amazon s3

--- guia_backend/settings.py
+++ guia_backend/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_api_key',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -164,3 +165,13 @@ TOKEN_EXPIRED_AFTER_TIME = 43200
 CORS_ALLOW_HEADERS = [
     '*'
 ]
+
+# AWS S3 settings
+# This connects the guia_backend to Amazon S3 guia-buckets
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+AWS_QUERYSTRING_AUTH = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
