b9539c18ccc87b696fd7846ee3cec72c0a0c659a
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-02-19 13:20:15 +0800

    fix admin

--- guia_db/admin/Artwork.py
+++ guia_db/admin/Artwork.py
@@ -3,7 +3,7 @@ from ..models.Artwork import Artwork
 
 class ArtworkAdmin(admin.ModelAdmin):
   list_display = ('art_id', 'title', 'get_section_name', 'medium', 'date_published', 'added_on', 'get_added_by_name', 'updated_on', 'get_updated_by_name', 'is_deleted')
-  search_fields = ('art_id', 'title', 'section_id__section_name', 'medium', 'date_published', 'admin_id__admin_username', 'updated_on', 'is_deleted')
+  search_fields = ('art_id', 'title', 'section_id__section_name', 'medium', 'date_published', 'added_by__admin_user', 'updated_on', 'is_deleted')
   ordering = ('art_id',)
 
   def get_section_name(self, obj):
@@ -11,11 +11,17 @@ class ArtworkAdmin(admin.ModelAdmin):
   get_section_name.short_description = "Section Name"
 
   def get_added_by_name(self, obj):
-    return obj.added_by.admin_username
+    if obj.added_by is None:
+      return ""
+    else:
+      return obj.added_by.user.username
   get_added_by_name.short_description = "Added By"
 
   def get_updated_by_name(self, obj):
-    return obj.updated_by.admin_username
+    if obj.updated_by is None:
+      return ""
+    else:
+      return obj.updated_by.user.username
   get_updated_by_name.short_description = "Updated By"
 
 admin.site.register(Artwork, ArtworkAdmin)
