c119cebfac1aafe6cf4ac224755001aea1b8781c
Daughpane Reponte <daughpanereponte@gmail.com>
2024-03-21 16:35:39 +0800

    added cors origin for azure

--- guia_backend/settings.py
+++ guia_backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_api_key',
     'rest_framework.authtoken',
+    'corsheaders',
     'storages'
 ]
 
@@ -167,6 +168,11 @@ TOKEN_EXPIRED_AFTER_TIME = 43200
 CORS_ALLOW_HEADERS = [
     '*'
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    "https://guia-backend.azurewebsites.net"
+]
+
 
 # AWS S3 settings
 # This connects the guia_backend to Amazon S3 guia-buckets
