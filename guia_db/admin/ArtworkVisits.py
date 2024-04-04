24b46baec666e89093236ff50490a0739c97d812
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-04-04 15:33:07 +0800

    add is_deleted on artwork_visits

--- guia_db/admin/ArtworkVisits.py
+++ guia_db/admin/ArtworkVisits.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from ..models.ArtworkVisits import ArtworkVisits
 
 class ArtworkVisitsAdmin(admin.ModelAdmin):
-  list_display = ('visit_id', 'visitor_id', 'get_visitor_museum_name','get_art_title', 'get_art_museum_name', 'art_visited_on', 'visit_type')
+  list_display = ('visit_id', 'visitor_id', 'get_visitor_token_preview', 'get_visitor_museum_name','get_art_title', 'get_art_museum_name', 'art_visited_on', 'visit_type', 'is_deleted')
   search_fields = ('visit_id', 'visitor_id', 'art_id__art_title', 'art_visited_on', 'visit_type')
   ordering = ('visit_id',)
 
@@ -17,5 +17,9 @@ class ArtworkVisitsAdmin(admin.ModelAdmin):
   def get_art_museum_name(self, obj):
     return obj.art_id.section_id.museum_id.museum_name
   get_art_museum_name.short_description = "Artwork Museum"
+
+  def get_visitor_token_preview(self, obj):
+    return obj.visitor_id.visitor_token[:20] + "..." if obj.visitor_id.visitor_token else ""
+  get_visitor_token_preview.short_description = "Visitor Token"
 
 admin.site.register(ArtworkVisits, ArtworkVisitsAdmin)
